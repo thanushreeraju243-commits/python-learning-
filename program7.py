ba81eaec75142fdd965a2486fcfb44d2dd3870a8
THANUSHREE RAJU <thanushreeraju243@gmail.com>
2026-09-17 22:09:09 +0530

    Add files via upload

---
+++ program7.py
@@ -0,0 +1,4 @@
+word = input("Enter a word: ")
+for letter in word:
+    if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
+        print(f"{letter} is a vowel.")      
---
+++ program9.py
@@ -0,0 +1,5 @@
+def square(num):
+    return num * num
+
+result = square(5)
+print(result)
